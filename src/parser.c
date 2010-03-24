ee3030ddcfd75f22798c5113fd2a1612e94712c8
Torbjörn Söderstedt <torbjrn@users.sourceforge.net>
2010-03-24 22:12:08 +0100

    Disallow ++ and -- operators for now.

--- src/parser.c
+++ src/parser.c
@@ -291,6 +291,7 @@ static uint parseExpression3(ParseState *state)
     skipWhitespace(state);
     if (readOperator(state, '+'))
     {
+        assert(!readOperator(state, '+'));
         skipWhitespace(state);
         value2 = parseExpression4(state);
         value = ParseStateWriteBinaryOperation(
@@ -298,6 +299,7 @@ static uint parseExpression3(ParseState *state)
     }
     else if (readOperator(state, '-'))
     {
+        assert(!readOperator(state, '-'));
         skipWhitespace(state);
         value2 = parseExpression4(state);
         value = ParseStateWriteBinaryOperation(
