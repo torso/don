de22b122b0e189a9f61890273c56c652104977e9
Torbjörn Söderstedt <torbjrn@users.sourceforge.net>
2010-10-07 18:43:00 +0200

    Make don able to build itself.

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#if gcc -O3 -ggdb3 -std=c89 -pedantic -Wno-error=unused-parameter -Wno-error=unused-variable -Wno-error=unused-function -Wall -Wextra -Wformat=2 -Winit-self -Wmissing-include-dirs -Wswitch-enum -Wsync-nand -Wunused -Wstrict-overflow=5 -Wfloat-equal -Wundef -Wshadow -Wbad-function-cast -Wc++-compat -Wcast-align -Wwrite-strings -Wconversion -Wlogical-op -Waggregate-return -Wstrict-prototypes -Wold-style-definition -Wmissing-prototypes -Wmissing-declarations -Wmissing-noreturn -Wmissing-format-attribute -Wnormalized=nfc -Wpacked -Wpacked-bitfield-compat -Wredundant-decls -Wnested-externs -Winline -Winvalid-pch -Wdisabled-optimization -Wstack-protector -pipe -march=native src/*.c -o don ; then
+if gcc -DDEBUG -O0 -ggdb3 -rdynamic -std=c89 -pedantic -Werror -Wno-error=unused-parameter -Wno-error=unused-variable -Wno-error=unused-function -Wall -Wextra -Wformat=2 -Winit-self -Wmissing-include-dirs -Wswitch-enum -Wsync-nand -Wunused -Wstrict-overflow=5 -Wfloat-equal -Wundef -Wshadow -Wunsafe-loop-optimizations -Wbad-function-cast -Wc++-compat -Wcast-align -Wwrite-strings -Wconversion -Wlogical-op -Waggregate-return -Wstrict-prototypes -Wold-style-definition -Wmissing-prototypes -Wmissing-declarations -Wmissing-noreturn -Wmissing-format-attribute -Wnormalized=nfc -Wpacked -Wpacked-bitfield-compat -Wredundant-decls -Wnested-externs -Wunreachable-code -Wno-error=unreachable-code -Winline -Winvalid-pch -Wdisabled-optimization -Wstack-protector -pipe -march=native src/*.c -o donbootstrap ; then
+
+./donbootstrap $@
+
+fi
