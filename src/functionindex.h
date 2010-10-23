1e2b2d5f00d723cb9a8f38c2a2be3ce020e8e26d
Torbjörn Söderstedt <torbjrn@users.sourceforge.net>
2010-10-23 17:58:59 +0200

    Remove outdated comment.

--- src/functionindex.h
+++ src/functionindex.h
@@ -1,24 +1,3 @@
-/*
-  This class holds information about functions (including targets).
-
-  The file, line number and file offset is used when parsing the function.
-
-  The argument names are used when parsing other functions. The function
-  signature is parsed eagerly, and the body lazily.
-
-  The bytecode offset is used when running a function. While parsing, the offset
-  will be the offset in the intermediate bytecode format.
-
-
-  To add a new function, call FunctionIndexBeginFunction. Then call
-  FunctionIndexAddArgument as many times as needed. When done, call
-  FunctionIndexFinishFunction.
-
-
-  Before looking up functions, the index needs to be built. After this has been
-  done, no more functions can be added.
-*/
-
 extern void FunctionIndexInit(void);
 extern void FunctionIndexDispose(void);
 
