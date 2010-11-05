34fbfcc139666bac1d88172e9d96a354b5b8f34f
Torbjörn Söderstedt <torbjrn@users.sourceforge.net>
2010-11-05 22:09:07 +0100

    Add more filename characters.

--- src/parser.c
+++ src/parser.c
@@ -73,7 +73,7 @@ static boolean isFilenameCharacter(byte c)
     return (c >= 'a' && c <= 'z') ||
         (c >= 'A' && c <= 'Z') ||
         (c >= '0' && c <= '9') ||
-        c == '/' || c == '.' || c == '*';
+        memchr("/.*-+~_=!@#$%^&", c, 15);
 }
 
 static attrprintf(2, 3) void error(ParseState *state, const char *format, ...)
